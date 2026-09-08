4d86698b360ff0b22b44c9e03f985978cdfc0aac
acrich0507-cloud <acrich0507@gmail.com>
2026-09-08 15:28:29 +0800

    Update config.js

--- config.js
+++ config.js
@@ -3,5 +3,5 @@ const CONFIG = {
     GAS_API_URL: "https://script.google.com/macros/s/AKfycby-4Ou7OgwVapyrKjVsgx6RWdPwuKTn0aIkdkrTLNw38qnoCLyHldGVOTzIK5gDWhyr7Q/exec",
     
     // 你的 LINE LIFF ID (建議一併抽離，因為首頁跟其他頁面可能都會用到)
-    LIFF_ID: "你的_LIFF_ID_請填入這裡" 
+    LIFF_ID: "2011433016-ReLRLT5q" 
 };
