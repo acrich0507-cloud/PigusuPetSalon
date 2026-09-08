85c9d335c5ec85f14528ef74ad448017b34af243
acrich0507-cloud <acrich0507@gmail.com>
2026-09-08 15:25:28 +0800

    Create config.js

---
+++ config.js
@@ -0,0 +1,7 @@
+const CONFIG = {
+    // 你的 GAS API 網址
+    GAS_API_URL: "https://script.google.com/macros/s/AKfycby-4Ou7OgwVapyrKjVsgx6RWdPwuKTn0aIkdkrTLNw38qnoCLyHldGVOTzIK5gDWhyr7Q/exec",
+    
+    // 你的 LINE LIFF ID (建議一併抽離，因為首頁跟其他頁面可能都會用到)
+    LIFF_ID: "你的_LIFF_ID_請填入這裡" 
+};
