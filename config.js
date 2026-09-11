6cc0f9323a1ec54790d1ddb1d385c85fa550cccc
acrich0507-cloud <acrich0507@gmail.com>
2026-09-11 16:35:31 +0800

    更新前端頁面

--- config.js
+++ config.js
@@ -1,11 +1,11 @@
 const CONFIG = {
     // 前端部署版本（每次發布請改這裡，用來確認 LIFF / 靜態檔是否已更新）
-    APP_VERSION: "2026.09.11.1",
+    APP_VERSION: "2026.09.11.2",
 
     // 你的 GAS API 網址
     GAS_API_URL: "https://script.google.com/macros/s/AKfycby-4Ou7OgwVapyrKjVsgx6RWdPwuKTn0aIkdkrTLNw38qnoCLyHldGVOTzIK5gDWhyr7Q/exec",
     
     // 你的 LINE LIFF ID (建議一併抽離，因為首頁跟其他頁面可能都會用到)
     LIFF_ID: "2011433016-ReLRLT5q" 
-    
+
 };
