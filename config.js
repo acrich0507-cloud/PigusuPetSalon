12f0fea04da5df69feaa7c039560b2ee3d4d6c8a
acrich0507-cloud <acrich0507@gmail.com>
2026-09-11 17:27:45 +0800

    更新前端頁面

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 const CONFIG = {
     // 前端部署版本（每次發布請改這裡，用來確認 LIFF / 靜態檔是否已更新）
-    APP_VERSION: "2026.09.11.2",
+    APP_VERSION: "2026.09.11.3",
 
     // 你的 GAS API 網址
     GAS_API_URL: "https://script.google.com/macros/s/AKfycby-4Ou7OgwVapyrKjVsgx6RWdPwuKTn0aIkdkrTLNw38qnoCLyHldGVOTzIK5gDWhyr7Q/exec",
